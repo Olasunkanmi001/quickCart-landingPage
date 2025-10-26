64d3e03403a24e8edf6cc0939640d2e0943cb310
Shana Adams <shana.adams10@gmail.com>
2025-10-26 14:42:51 -0600

    Add Footer to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './component/header'
-import Hero from './component/hero'
-import Marketplace from './component/marketPlace'
-import Howitworks from './component/howitworks'
-import Swiper from './component/swiper'
-import Vendor from './component/vendor'
-import Info from './component/infoSection'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/header";
+import Hero from "./component/hero";
+import Marketplace from "./component/marketPlace";
+import Howitworks from "./component/howitworks";
+import Swiper from "./component/swiper";
+import Vendor from "./component/vendor";
+import Info from "./component/infoSection";
+import Footer from "./component/footer";
 
 export default function App() {
   return (
-   <div>
-
-    <Header/>
-    <Hero/>
-    <Howitworks/>
-    <Marketplace/>
-    <Vendor/>
-    <Info/>
-    <Swiper/>
-   
-
-
-
-
-   </div>
-  
-  )
+    <div>
+      <Header />
+      <Hero />
+      <Howitworks />
+      <Marketplace />
+      <Vendor />
+      <Info />
+      <Swiper />
+      <Footer />
+    </div>
+  );
 }
