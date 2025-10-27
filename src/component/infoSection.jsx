8a66181e175bab2cb2872d642007c48a084e87fc
Shana Adams <shana.adams10@gmail.com>
2025-10-26 22:08:12 -0600

    Add animation to Become A Vendor bttn.

--- src/component/infoSection.jsx
+++ src/component/infoSection.jsx
@@ -98,7 +98,7 @@ const QuickCartInfoSection = () => {
           </ul>
           <Link
             to="/vendorLanding"
-            className="bg-black text-white px-5 py-2 rounded-lg text-sm hover:bg-gray-800 inline-block transition-colors"
+            className="bg-black text-white px-5 py-2 rounded-lg text-sm hover:bg-gray-800 inline-block active:scale-95 transition-all duration-150"
           >
             Become a Vendor
           </Link>
