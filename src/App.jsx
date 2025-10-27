60435f1c8204f1383f464db4647ca3b9006d28a0
Shana Adams <shana.adams10@gmail.com>
2025-10-26 22:12:12 -0600

    Create Layout component. So header and footer are on each page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./component/Layout";
 import Home from "./component/Home";
 import VendorLanding from "./component/vendorLanding";
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vendorLanding" element={<VendorLanding />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vendorLanding" element={<VendorLanding />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
