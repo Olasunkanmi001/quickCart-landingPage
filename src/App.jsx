803835741c930c812edacb9bfc4650068980b38e
Shana Adams <shana.adams10@gmail.com>
2025-10-28 13:01:09 -0600

    Setup route for riderLanding.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./component/Layout";
 import Home from "./component/Home";
 import VendorLanding from "./component/vendorLanding";
+import RiderLanding from "./component/riderLanding";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/vendorLanding" element={<VendorLanding />} />
+          <Route path="/riderLanding" element={<RiderLanding />} />
         </Routes>
       </Layout>
     </Router>
