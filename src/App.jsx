70b13333f43b0536bbc8b1f073c3ad950edc5f24
Shana Adams <shana.adams10@gmail.com>
2025-10-29 16:59:30 -0600

    Add routes for /buyerSignUp - User registration /buyerLogin - User login /buyerDashboard - Authenticated user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Layout from "./component/Layout";
 import Home from "./component/Home";
 import VendorLanding from "./component/vendorLanding";
 import RiderLanding from "./component/riderLanding";
+import BuyerSignUp from "./component/buyerSignUp";
+import BuyerLogin from "./component/buyerLogin";
+import BuyerDashboard from "./component/buyerDashboard";
 
 export default function App() {
   return (
@@ -12,6 +15,9 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/vendorLanding" element={<VendorLanding />} />
           <Route path="/riderLanding" element={<RiderLanding />} />
+          <Route path="/buyerSignUp" element={<BuyerSignUp />} />
+          <Route path="/buyerLogin" element={<BuyerLogin />} />
+          <Route path="/buyerDashboard" element={<BuyerDashboard />} />
         </Routes>
       </Layout>
     </Router>
